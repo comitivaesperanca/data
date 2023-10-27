aba852a52c7cd75d89af97949613b49db5341d5e
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2023-10-27 02:17:47 -0300

    fix: atualização da API para baixar o modelo no primeiro startup

--- predict-api/app/main.py
+++ predict-api/app/main.py
@@ -7,6 +7,13 @@ from entities.error_message import error_message
 from repositories.predict import predict_text
 from fastapi.middleware.cors import CORSMiddleware
 
+import urllib.request
+import os
+
+model_url = "https://huggingface.co/mfelipemota/comitivaesperanca-soja-model/resolve/main/modelo_soja.pt"
+model_path = "models/modelo_redeneural_bert_soja.pt"
+
+
 # tags
 tags_metadata = [
     {
@@ -22,31 +29,63 @@ app = FastAPI(
     openapi_tags=tags_metadata,
 )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*']
-)
+app.add_middleware(CORSMiddleware, allow_origins=["*"])
 
-@app.get("/predict", tags=["predict"], summary="Predict de texto", description="""Endpoint para realizar predições
+
+@app.on_event("startup")
+async def startup_event():
+    if not os.path.exists(model_path):
+        urllib.request.urlretrieve(model_url, model_path)
+
+
+@app.get(
+    "/predict",
+    tags=["predict"],
+    summary="Predict de texto",
+    description="""Endpoint para realizar predições
         utilizando modelo Naive Bayes  ou Rede Neural (baseado em BERT) para 
         realizar o predict do sentimento do textículo encaminhado como parâmetro.""",
-        response_description="Predict news category", response_model= Union[predict_content, error_message])
-async def predict(text_content: str, model_type: str = "naive_bayes") -> Union[str, float]:
+    response_description="Predict news category",
+    response_model=Union[predict_content, error_message],
+)
+async def predict(
+    text_content: str, model_type: str = "naive_bayes"
+) -> Union[str, float]:
     if text_content is None:
         return {"message": "É necessário enviar um texto para realizar a predição."}
     if model_type is None:
-        return {"message": "É necessário enviar um modelo para realizar a predição. Utilize naive-bayes ou rede-neural"}
-    if model_type in ['naive_bayes', 'rede_neural']:
+        return {
+            "message": "É necessário enviar um modelo para realizar a predição. Utilize naive-bayes ou rede-neural"
+        }
+    if model_type in ["naive_bayes", "rede_neural"]:
         return await predict_text(text_content=text_content, model_type=model_type)
     else:
         # return status code 401
         response.status_code = status.HTTP_401_UNAUTHORIZED
-        return {"message": "É necessário enviar um modelo para realizar a predição. Utilize naive-bayes ou rede-neural"}
-@app.get("/metrics", tags=["predict"], summary="Métricas", description="""Endpoint para realizar métricas da API.""", response_description="Métricas", response_model= str)
+        return {
+            "message": "É necessário enviar um modelo para realizar a predição. Utilize naive-bayes ou rede-neural"
+        }
+
+
+@app.get(
+    "/metrics",
+    tags=["predict"],
+    summary="Métricas",
+    description="""Endpoint para realizar métricas da API.""",
+    response_description="Métricas",
+    response_model=str,
+)
 async def metrics() -> str:
     return "Métricas da API de predição de sentimento de textos"
 
-@app.get("/healthcheck", tags=["predict"], summary="Healthcheck", description="""Endpoint para realizar healthcheck da API.""",
-        response_description="Healthcheck", response_model= str)
+
+@app.get(
+    "/healthcheck",
+    tags=["predict"],
+    summary="Healthcheck",
+    description="""Endpoint para realizar healthcheck da API.""",
+    response_description="Healthcheck",
+    response_model=str,
+)
 async def healthcheck() -> str:
     return "API de predição de sentimento de textos"
